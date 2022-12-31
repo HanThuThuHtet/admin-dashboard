51ce2adb7cb28362c8cfef66e4a92357cca71e32
hanthuthuhtet <htth5499@gmail.com>
2022-12-31 12:53:09 +0630

    layout

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import './style.scss'
+import * as bootstrap from 'bootstrap';
+
+let showSidebar = document.getElementById('showSidebar');
+let sidebar  = document.getElementById('sidebar');
+showSidebar.addEventListener('click',function(){
+    //console.log("show");
+    sidebar.classList.toggle("show");
+})
